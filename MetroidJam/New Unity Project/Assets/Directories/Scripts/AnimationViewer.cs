67d58ef2a21d56fb149530939bad7e4a4f3445f8
Kostas Man <anthosnet@hotmail.com>
2018-06-05 00:27:47 +0300

    Animation clips of Hero
    Press 1-4 to view animations
    Testing Animations Scene

---
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/AnimationViewer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationViewer : MonoBehaviour {
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("WakingUP");
+        }
+
+        if (Input.GetKeyDown("2") )
+        {
+            anim.Play("Walk_boxers");
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            anim.Play("PickUpTable");
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            anim.Play("PickUpFloor");
+        }
+
+
+    }
+}
