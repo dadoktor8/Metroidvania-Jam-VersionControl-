fdbb57ae27c224d4cc76f9703e026599881a49a4
dadoktor8 <dadoktor8@gmail.com>
2018-06-05 23:38:53 +0530

    I created a Demo Version.
    This has an idea of how the in game mechanics will feel like.

---
+++ MetroidJam/New Unity Project/Assets/Directories/Version1.0/VersionScripts/WalkScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+namespace walkAnim{
+public class WalkScript : MonoBehaviour {
+
+		Animator Anim; 
+		SpriteRenderer SpriteBoi; 
+
+
+	void Start () {
+		
+			Anim = GetComponent<Animator> (); 
+
+			SpriteBoi = GetComponent<SpriteRenderer> (); 
+
+	}
+	
+	
+	void Update () 
+		{
+
+			Movement ();
+			{
+
+				float move = Input.GetAxis ("Horizontal"); 
+				Anim.SetFloat ("Speed", move); 
+
+			}
+
+		
+	}
+
+
+		void Movement()
+		{
+			if (Input.GetKey (KeyCode.D)) {
+
+				SpriteBoi.flipX = false; 
+				MovementRight (); 
+		
+
+			}
+
+			if (Input.GetKey (KeyCode.A)) {
+
+				SpriteBoi.flipX = true; 
+				MovementRight (); 
+
+
+
+			}
+		}
+
+
+			void MovementRight () 
+			{
+
+				transform.Translate (Vector2.right * 3f * Time.deltaTime);
+				transform.eulerAngles = new Vector2 (0, 0); 
+				
+
+			}
+
+
+
+
+
+		}
+
+}
---
+++ MetroidJam/New Unity Project/Assets/Directories/Version1.0/VersionScripts/WalkingNewScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingNewScript : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody; 
+
+	Animator anim; 
+
+	[SerializeField]
+	private float movementSpeed; 
+
+	private bool facingRight ; 
+
+	void Start () {
+
+		myRigidbody = GetComponent<Rigidbody2D> (); 
+
+		anim = GetComponent<Animator> (); 
+
+		facingRight = true; 
+
+	}
+	
+
+	void FixedUpdate () 
+	{
+		float horizontal = Input.GetAxis("Horizontal"); 
+
+		HandleMovement (horizontal); 
+
+		Flip (horizontal); 
+	}
+
+
+	private void HandleMovement(float horizontal)
+	{
+		    
+		   
+		myRigidbody.velocity = new Vector2(horizontal * movementSpeed ,myRigidbody.velocity.y); 
+
+		anim.SetFloat ("speed", Mathf.Abs( horizontal)); 
+		
+	}
+
+
+	private void Flip (float horizontal)
+	{
+
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) 
+		{
+
+			facingRight = !facingRight; 
+
+			Vector3 theScale = transform.localScale; 
+
+			theScale.x *= -1; 
+
+			transform.localScale = theScale; 
+
+		}
+	}
+
+
+}
