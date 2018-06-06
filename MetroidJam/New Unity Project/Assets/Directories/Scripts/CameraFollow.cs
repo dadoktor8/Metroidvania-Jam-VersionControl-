e80f1c206b35537ea2bf64ea82ce752a628d21a1
dadoktor8 <dadoktor8@gmail.com>
2018-06-06 21:33:59 +0530

    Made some changes.
    Please back up before you pull.

--- MetroidJam/New Unity Project/Assets/Directories/Scripts/CameraFollow.cs
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/CameraFollow.cs
@@ -3,13 +3,21 @@
 public class CameraFollow : MonoBehaviour {
 
 	public Transform Playertarget; 
+	  
 
 	public float smoothSpeed = 0.125f; 
 	public Vector3 offset; 
+	public float stopFollowtime = 0.5f; 
+
+
+
+
 
 	void LateUpdate ()
 	{
 
+
+	
 		Vector3 desiredPosition = Playertarget.position + offset; 
 		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed); 
 		transform.position = smoothedPosition;
@@ -17,5 +25,15 @@ public class CameraFollow : MonoBehaviour {
 
 		transform.LookAt (Playertarget);  	
 
+
+
+	
+
+
 	}
+
+
+
+
+
 }
