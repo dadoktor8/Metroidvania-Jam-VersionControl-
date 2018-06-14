db50bff907a23f10d58aa43529959b0b448f7e92
dadoktor8 <dadoktor8@gmail.com>
2018-06-14 22:34:21 +0530

    Added an enemy, AI in works

---
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/EnemyAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations; 
+
+public class EnemyAI : MonoBehaviour {
+
+
+	Animator enemyAnime; 
+
+	Rigidbody2D rigidBlob; 
+
+	float enemyTriggerDistance = 3f; 
+
+	float enemyAttackDistance = 1f; 
+
+	float speed = 4f; 
+
+	public Transform targetProtagonist;  
+
+	void Start () {
+		//enemyAnime = GetComponent<Animator> (); 
+		rigidBlob = GetComponent<Rigidbody2D> (); 
+		targetProtagonist = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> (); 
+	}
+	
+
+	void Update () 
+	{
+		if (Vector2.Distance (transform.position, targetProtagonist.position) > 1) {
+
+			transform.position = Vector2.MoveTowards (transform.position, targetProtagonist.position, speed * Time.deltaTime); 
+
+		}
+
+	}
+}
