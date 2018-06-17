62660c3b6791ea4be358d65ce9bd0a84cdbd562d
dadoktor8 <dadoktor8@gmail.com>
2018-06-17 23:53:05 +0530

    Seamless Movement added.
    Added movement

---
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/BlobmanAI.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobmanAI : MonoBehaviour {
+
+	public Transform[] patrolPoints; 
+	int currentPoint ; 
+	public float speed = 0.5f; 
+	public float timeStill = 1f;
+	public float sight = 3f; 
+	public float force = 100f; 
+	Animator anim; 
+
+
+
+	void Start () {
+		StartCoroutine ("Patrol"); 
+		anim = GetComponent<Animator> (); 
+
+	}
+	
+
+	void Update () {
+
+		RaycastHit2D projectileScan = Physics2D.Raycast (transform.position, transform.localScale.x * Vector2.right, sight); 
+
+		if (projectileScan.collider != null  && (projectileScan.collider.tag == "Player"))
+			
+
+
+				GetComponent<Rigidbody2D> ().AddForce (Vector3.up*force + projectileScan.collider.transform.position - transform.position);
+
+
+
+		
+
+			
+
+		
+	}
+
+
+	void OnDrawGizmos ()
+	{
+
+
+		Gizmos.color = Color.red; 
+
+		Gizmos.DrawLine (transform.position, transform.transform.position + transform.localScale.x * Vector3.right * sight);
+
+
+	}
+
+
+
+	IEnumerator Patrol()
+	{
+
+		while (true) 
+		{
+
+			if (transform.position.x == patrolPoints [currentPoint].position.x)
+			{
+
+				currentPoint++; 
+
+				anim.SetBool ("Walking", false); 
+
+				yield return new WaitForSeconds (timeStill); 
+
+				anim.SetBool ("Walking", true); 
+			}
+
+			if(currentPoint >=patrolPoints.Length)
+			{
+
+				currentPoint = 0; 
+
+			}
+
+
+
+			transform.position = Vector2.MoveTowards(transform.position , new Vector2(patrolPoints[currentPoint].position.x,transform.position.y),speed * Time.deltaTime ); 
+
+
+			if (transform.position.x > patrolPoints [currentPoint].position.x) {
+
+				transform.localScale = new Vector3 (-1, 1, 1); 
+
+
+			} else if (transform.position.x > patrolPoints [currentPoint].position.x)
+			{
+
+				transform.localScale = new Vector3 (1, 1, 1); 
+			}
+
+
+
+
+			yield return null; 
+
+		}
+
+	
+	
+	
+	}
+
+
+
+}
--- MetroidJam/New Unity Project/Assets/Directories/Scripts/EnemyAI.cs
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/EnemyAI.cs
@@ -10,28 +10,57 @@ public class EnemyAI : MonoBehaviour {
 
 	Rigidbody2D rigidBlob; 
 
-	float enemyTriggerDistance = 3f; 
+	//float enemyTriggerDistance = 3f; 
 
-	float enemyAttackDistance = 1f; 
+	//float enemyAttackDistance = 1f; 
 
-	float speed = 4f; 
+	float speed = 1f; 
+
+	private bool movingRight = true; 
+
+	public Transform groundDetection; 
 
 	public Transform targetProtagonist;  
 
 	void Start () {
-		//enemyAnime = GetComponent<Animator> (); 
+		
 		rigidBlob = GetComponent<Rigidbody2D> (); 
 		targetProtagonist = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> (); 
 	}
 	
 
-	void Update () 
+	void Update ()
 	{
-		if (Vector2.Distance (transform.position, targetProtagonist.position) > 1) {
 
-			transform.position = Vector2.MoveTowards (transform.position, targetProtagonist.position, speed * Time.deltaTime); 
+		 transform.Translate (Vector2.right * speed * Time.deltaTime); 
+
+		RaycastHit2D groundInfo = Physics2D.Raycast (groundDetection.position, Vector2.down, 1f);
+
+
+		if (groundInfo.collider == false)
+		{
+
+			if (movingRight == true) {
+
+				transform.eulerAngles = new Vector3 (0, -180, 0); 
+
+				movingRight = false; 
+
+			} else 
+			{
+
+				transform.eulerAngles = new Vector3 (0, 0, 0); 
+				movingRight = true; 
+			}
+
+
+				if (Vector2.Distance (transform.position, targetProtagonist.position) > 1) {
+
+					transform.position = Vector2.MoveTowards (transform.position, targetProtagonist.position, speed * Time.deltaTime);
+
+				}
+			}
 
 		}
-
-	}
 }
+
