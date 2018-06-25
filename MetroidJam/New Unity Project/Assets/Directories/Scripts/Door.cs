dd3abc2986a9bbb9960f50c873637697475e9908
Kostas Man <anthosnet@hotmail.com>
2018-06-25 21:40:18 +0300

    Transition in Grids
    Transision, new tileset, no glitches!
    Template_EP_64

---
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/Door.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField] GameObject currentGrid;
+    [SerializeField] GameObject nextGrid;
+    [SerializeField] float fadeWait = 0.5f;
+    [SerializeField] Animator anim;
+
+    [SerializeField] GameObject text;
+
+    // Use this for initialization
+    void Start () {
+       
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            text.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("Press X to open!");
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                anim.SetBool("FadeIn", true);
+                StartCoroutine(FadeTo() );
+                
+            }
+            
+        }
+    }
+
+    IEnumerator FadeTo()
+    {
+        yield return new WaitForSeconds(fadeWait);
+        anim.SetBool("FadeIn", false);
+        currentGrid.SetActive(false);
+        nextGrid.SetActive(true);
+    }
+}
