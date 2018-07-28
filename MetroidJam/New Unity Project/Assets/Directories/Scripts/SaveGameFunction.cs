68845dc612a37a5563406f59cc25e2f3d143ed2e
dadoktor8 <dadoktor8@gmail.com>
2018-07-28 22:14:33 +0530

    SaveFunction(Beta)

---
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/SaveGameFunction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGameFunction : MonoBehaviour {
+
+
+	void Update()
+	{
+
+		if (Input.GetKeyDown(KeyCode.P)) 
+		{
+			SaveManager.saveManager.Delete (); 
+			
+		}
+
+		if (Input.GetKeyDown (KeyCode.L)) 
+		{
+			SaveManager.saveManager.playerPositionX = transform.position.x; 
+			SaveManager.saveManager.playerPositionY = transform.position.y; 
+
+
+		}
+
+
+		if (Input.GetKeyDown (KeyCode.K)) 
+		{
+			SaveManager.saveManager.Load (); 
+			transform.position = new Vector2 (
+			SaveManager.saveManager.playerPositionX, 
+			SaveManager.saveManager.playerPositionY 
+			); 
+		}
+
+
+
+	}
+
+}
---
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/SaveManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary; 
+using System; 
+using System.IO; 
+using UnityEngine;
+using UnityEditor.SceneManagement; 
+
+public class SaveManager : MonoBehaviour {
+
+	public static SaveManager saveManager; 
+
+	public float playerPositionX; 
+	public float playerPositionY; 
+
+
+
+	void Awake ()
+	{
+		if (saveManager == null) {
+			DontDestroyOnLoad (gameObject);
+			saveManager = this; 
+
+
+		} else if (saveManager != this) {
+
+			Destroy (gameObject); 
+
+
+		}
+	}
+		public void Save()
+		{ 
+
+		BinaryFormatter bf = new BinaryFormatter (); 
+		FileStream file = File.Create (Application.persistentDataPath + "/playerInfo.dat"); 
+
+		playerData data = new playerData (); 
+		data.playerPosX = playerPositionX; 
+		data.playerPosY = playerPositionY; 
+
+
+		bf.Serialize (file, data); 
+		file.Close (); 
+
+	}
+
+	public void Load()
+	{
+
+
+		if (File.Exists (Application.persistentDataPath + "/playerInfo.dat")) 
+		{
+			BinaryFormatter bf = new BinaryFormatter (); 
+			FileStream file = File.Open (Application.persistentDataPath + "/playerIndo.dat", FileMode.Open); 
+
+
+			playerData data = (playerData)bf.Deserialize (file); 
+			file.Close (); 
+
+			playerPositionX = data.playerPosX; 
+			playerPositionY = data.playerPosY;
+
+		}
+
+
+	}
+
+
+	public void Delete()
+	{
+
+		if (File.Exists (Application.persistentDataPath + "/playerInfo.dat"))
+		{
+
+			File.Delete (Application.persistentDataPath + "/playerInfo.dat"); 
+
+		}
+
+
+
+	}
+
+
+
+
+	}
+
+
+[Serializable]
+
+class playerData
+{
+	public float playerPosX; 
+	public float playerPosY; 
+
+
+}
+
+
