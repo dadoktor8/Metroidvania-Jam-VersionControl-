f51277aed2af82b02c3e285ba846d839dc6fa6a0
Kostas Man <anthosnet@hotmail.com>
2018-08-09 21:25:01 +0300

    BoSS Fight Updated
    Respaning waves of enemies in afixed time

--- MetroidJam/New Unity Project/Assets/Directories/Scripts/EnemySpawner.cs
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/EnemySpawner.cs
@@ -15,36 +15,65 @@ public class EnemySpawner : MonoBehaviour {
     [SerializeField]
     int EnemyLimit = 0;
     
+    [SerializeField]
+    float RespawnTime = 13.0f;
+
     int Counter = 0;
 
     // Use this for initialization
     void Start () {
 
         StartCoroutine(Spawner());
-
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Counter <= EnemyLimit)
+        if (Counter == (EnemyLimit - 1) )
         {
-            // InvokeRepeating("Spawner", 1.0f, 5.0f);
+            //StartCoroutine(Reset());
+            ///InvokeRepeating("ResetInvoke", 10.0f,10.0f);
             
         }
+
+        
 
     }
 
     IEnumerator Spawner()
     {
-        while (Counter < EnemyLimit)
+        while (true)
         {
             enemyBlobman.gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.0f);
             Instantiate(enemyBlobman, spawnPoint.position, spawnPoint.rotation);
-            Instantiate(enemyBlobman, spawnPoint1.position, spawnPoint1.rotation);
-            Counter++;
             yield return new WaitForSeconds(3.0f);
+            Instantiate(enemyBlobman, spawnPoint1.position, spawnPoint1.rotation);  
+            yield return new WaitForSeconds(3.0f);
+            Counter++;
 
+            if (Counter >= EnemyLimit)
+            {
+                Counter = 0;
+                yield return new WaitForSeconds(RespawnTime);
+                
+            }
+            
         }
+    }
+
+    IEnumerator Reset()
+    {
+        Debug.Log("Respawning in...");
+        yield return new WaitForSeconds(2.9f);
+        Counter = 0;
+        
+    }
+
+    void ResetInvoke()
+    {
+        Debug.Log("Respawning in...");
+        Counter = 0;
+
     }
 }
