de814e7467f46fbc25f359a64fb42ece2eb5433e
Kostas Man <anthosnet@hotmail.com>
2018-08-11 11:07:24 +0300

    Final[BugFix]

---
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/ReturnToMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ReturnToMenu : MonoBehaviour {
+
+    [SerializeField]
+    int menuIndex = 0;
+
+   public void LoadMenu()
+    {
+        SceneManager.LoadScene(menuIndex);
+    }
+}
