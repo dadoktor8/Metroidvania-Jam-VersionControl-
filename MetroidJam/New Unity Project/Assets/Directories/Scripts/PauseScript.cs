282341582d5953139accc11239866a43f7506287
dadoktor8 <dadoktor8@gmail.com>
2018-08-15 14:25:05 +0530

    Added pause menu scripts.
    The game can now be paused

---
+++ MetroidJam/New Unity Project/Assets/Directories/Scripts/PauseScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+
+	public static bool gameIsPaused = false; 
+	public GameObject pauseMenuUI; 
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (gameIsPaused)
+			{
+				Resume (); 
+			} else
+			{
+
+				Pause ();
+			}
+		}
+	}
+
+	public void Resume() 
+	{
+
+		pauseMenuUI.SetActive (false); 
+		Time.timeScale = 1f; 
+		gameIsPaused = false; 
+
+
+
+	}
+
+	public void Pause()
+	{
+		pauseMenuUI.SetActive (true); 
+		Time.timeScale = 0f; 
+		gameIsPaused = true; 
+
+
+	}
+
+
+
+
+	public void QuitGame()
+	{
+		Application.Quit (); 
+
+	}
+
+
+}
